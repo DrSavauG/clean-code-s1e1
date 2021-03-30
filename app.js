8c153344eded3e531933c0ad23e96a90a856a430
DrSavauG <dr.savaug@gmail.com>
2021-03-30 18:10:40 +0300

    refactor: split to additional file

--- app.js
+++ app.js
@@ -203,18 +203,3 @@ for (let i = 0; i < completedTasksHolder.children.length; i++) {
 
 //Change edit to save when you are in edit mode.
 
-//#region ссылка на youtube
-const linkYouTube = document.querySelector('.links__youtube');
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://drsavaug.github.io/links/src.json');
-xhr.send();
-xhr.onload = function () {
-  if (xhr.status != 200) {
-    return;
-  }
-};
-xhr.onprogress = function (event) {
-  linkYouTube.href = JSON.parse(event.target.responseText)['clean-code-s1e1'];
-  console.log(linkYouTube.href);
-};
-//#endregion
